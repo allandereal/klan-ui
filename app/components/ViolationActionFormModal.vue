e627116c290cafea4bb8ae34ee3b17ae79d11b2f
Allan <ahullan@gmail.com>
2025-04-03 14:08:36 +0300

    add form in modal to capture actions taken

---
+++ app/components/ViolationActionFormModal.vue
@@ -0,0 +1,21 @@
+<script setup lang="js">
+defineProps({
+  violation: Object
+})
+
+const emit = defineEmits({close: Boolean })
+</script>
+
+<template>
+  <UModal
+      :close="{ onClick: () => emit('close', false) }"
+      :title="`${violation.id}`"
+  >
+    <template #footer>
+      <div class="flex gap-2">
+        <UButton color="neutral" label="Dismiss" @click="emit('close', false)" />
+        <UButton label="Success" @click="emit('close', true)" />
+      </div>
+    </template>
+  </UModal>
+</template>
